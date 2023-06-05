b6a2a0caacee881373a888f78b82bfce369d7614
Hakoyu <Hakoyu@outlook.com>
2023-06-06 05:26:00 +0800

    LocateOn原型

--- AutoGUI/Program.cs
+++ AutoGUI/Program.cs
@@ -1,9 +1,9 @@
 ﻿using System.Buffers;
-using System.Drawing;
 using System.Drawing.Imaging;
 using System.Runtime.InteropServices;
 using HKW.AutoGUI.AutoGUI;
 using HKW.AutoGUI.Native.Windows;
+using HKW.AutoGUI.Screen;
 using SixLabors.ImageSharp.Advanced;
 
 namespace HKW.AutoGUI;
@@ -13,16 +13,6 @@ internal class Program
     public static void Main(string[] args)
     {
 #if DEBUG
-        //_ = Parallel.For(
-        //    0,
-        //    100,
-        //    y =>
-        //    {
-        //        var array = ArrayPool<int>.Shared.Rent(10);
-        //        Console.WriteLine(y);
-        //        ArrayPool<int>.Shared.Return(array);
-        //    }
-        //);
         //HKWAutoGUI.Default.Mouse.AbsoluteMoveTo(30000, 30000, 1000);
         //string file = @"C:\Users\HKW\Desktop\1.bmp";
         //System.Diagnostics.Stopwatch stopWatch = new();
@@ -32,8 +22,17 @@ internal class Program
         //Console.WriteLine(HKWAutoGUI.Default.Mouse.MoveTo(201, 201).OnScreen(100, 100, 100, 100));
         //stopWatch.Stop();
         //Console.WriteLine($"\nSTOP {stopWatch.ElapsedMilliseconds:f4}ms");
-        string file = @"C:\Users\HKW\Desktop\1.bmp";
-        WinGDI32.CaptureScreen().ToImageSharp().Save(file);
+        //string file = @"C:\Users\HKW\Desktop\1.bmp";
+        //string file = @"C:\Users\HKW\Desktop\TestPicture\test1.bmp";
+        //DesktopCapture.DesktopDuplicator dd = new();
+        //var bitmap = dd.GetLatestFrame(out _);
+        //foreach (int i in Enumerable.Range(0, 1000))
+        //{
+        //    using var image = bitmap.ToImageSharp();
+        //    Console.WriteLine(image.Width);
+        //    Thread.Sleep(10);
+        //}
+        //WinGDI32.CaptureScreen().ToImageSharp().Save(file);
         //HKWAutoGUI.Default.Screen.Screenshot().Save(file);
         //DesktopDuplicator dd = new();
         //var bmp = dd.GetLatestFrame(out _);
@@ -42,12 +41,89 @@ internal class Program
         //bmp.Save(ms, ImageFormat.Bmp);
         //SixLabors.ImageSharp.Image.Load(ms.ToArray()).SaveAsBmp(file);
         //Image image = Image.Load(bmp.LockBits().);
-
+        string locateSoureImageFile = @"C:\Users\HKW\Desktop\TestPicture\LocateSoureImage1.png";
+        string locateDestImageFile = @"C:\Users\HKW\Desktop\TestPicture\LocateDestImage.png";
+        using var soureImage = Image.Load(locateSoureImageFile);
+        using var destImage = Image.Load(locateDestImageFile);
+        var locate = LocateOn(soureImage, destImage);
+        Console.WriteLine(locate);
 #endif
     }
 
 #if DEBUG
-
+    public static LocateData? LocateOn(Image sourceImage, Image destImage)
+    {
+        if (sourceImage.Width < destImage.Width || sourceImage.Height < destImage.Height)
+            throw new ArgumentException(
+                $"{nameof(sourceImage)} must be larger than {nameof(destImage)}"
+            );
+        var sourceGRAYImage = sourceImage.CloneAs<Abgr32>();
+        var destGRAYImage = destImage.CloneAs<Abgr32>();
+        sourceGRAYImage.Mutate(p => p.Grayscale());
+        destGRAYImage.Mutate(p => p.Grayscale());
+        var destY = 0;
+        var destRows = destGRAYImage.DangerousGetPixelRowMemory(destY).Span;
+        int locateX = -1;
+        int locateY = -1;
+        for (int y = 0; y < sourceGRAYImage.Height; y++)
+        {
+            var sourceRows = sourceGRAYImage.DangerousGetPixelRowMemory(y).Span;
+            int x = 0;
+            for (; x < sourceGRAYImage.Width; x++)
+            {
+                bool getOut = false;
+                int dx = 0;
+                if (locateX > -1 && destGRAYImage.Height <= sourceGRAYImage.Height - x)
+                {
+                    destRows = destGRAYImage.DangerousGetPixelRowMemory(++destY).Span;
+                    x = locateX;
+                    for (; dx < destGRAYImage.Width; dx++, x++)
+                    {
+                        if (sourceRows[x] != destRows[dx])
+                            break;
+                    }
+                    if (dx < destGRAYImage.Width)
+                    {
+                        x = locateX;
+                        y = locateY;
+                        locateX = -1;
+                        locateY = -1;
+                        destRows = destGRAYImage.DangerousGetPixelRowMemory(destY = 0).Span;
+                        getOut = true;
+                    }
+                    else
+                        getOut = true;
+                    if (destY == destGRAYImage.Height - 1)
+                        return new(locateX, locateY, destGRAYImage.Width, destGRAYImage.Height);
+                }
+                else if (
+                    locateX == -1
+                    && sourceRows[x] == destRows[0]
+                    && destGRAYImage.Width <= sourceGRAYImage.Width - x
+                )
+                {
+                    locateX = x;
+                    locateY = y;
+                    for (; dx < destGRAYImage.Width; dx++, x++)
+                    {
+                        if (sourceRows[x] != destRows[dx])
+                            break;
+                    }
+                    if (dx < destGRAYImage.Width)
+                    {
+                        x = locateX;
+                        locateX = -1;
+                        locateY = -1;
+                    }
+                    else
+                        getOut = true;
+                }
+                if (getOut)
+                    break;
+            }
+        }
+        return null;
+    }
 #endif
 }
 
--- AutoGUI/Screen/locateData.cs
+++ AutoGUI/Screen/locateData.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,7 +10,8 @@ namespace HKW.AutoGUI.Screen;
 /// <summary>
 /// 定位数据
 /// </summary>
-public struct LocateData
+[DebuggerDisplay("X = {X}, Y = {Y}, Width = {Width}, Height = {Height}")]
+public readonly struct LocateData
 {
     /// <summary>
     /// X轴坐标
@@ -30,4 +32,26 @@ public struct LocateData
     /// 高
     /// </summary>
     public int Height { get; }
+
+    /// <inheritdoc/>
+    /// <param name="x">X轴坐标</param>
+    /// <param name="y">Y轴坐标</param>
+    /// <param name="width">宽</param>
+    /// <param name="height">高</param>
+    public LocateData(int x, int y, int width, int height)
+    {
+        X = x;
+        Y = y;
+        Width = width;
+        Height = height;
+    }
+
+    /// <summary>
+    /// 转换为字符串信息
+    /// </summary>
+    /// <returns>字符串信息</returns>
+    public override string ToString()
+    {
+        return $"X = {X}, Y = {Y}, Width = {Width}, Height = {Height}";
+    }
 }
